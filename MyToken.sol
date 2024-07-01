bb6c5375c579256db58697c7adc9994c9892c40b
althealouise <87533603+althealouise@users.noreply.github.com>
2024-07-01 09:27:03 +0800

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20{
+    constructor() ERC20("AltCz", "ALC") {
+        _mint (msg.sender, 100 * 10 ** decimals());
+    }
+}
