e8ab95ec89dac2d577cf6617b8819d1e00e967a1
althealouise <87533603+althealouise@users.noreply.github.com>
2024-07-12 01:03:46 +0800

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -2,9 +2,22 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20{
-    constructor() ERC20("AltCz", "ALC") {
-        _mint (msg.sender, 100 * 10 ** decimals());
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20("AltCz", "ALC") Ownable(msg.sender) {
+        _mint(msg.sender, 100 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        return super.transfer(recipient, amount);
     }
 }
